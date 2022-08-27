c8c63047c919d81317e3426e066cac9b2e650898
Tendai <Tendai.Charura@gmail.com>
2022-08-27 11:26:24 +0200

    Added tmp CLI test command for testing purposes

--- gcalcli/cli/cli.py
+++ gcalcli/cli/cli.py
@@ -8,18 +8,27 @@ app = typer.Typer()
 
 
 @app.command()
-def set_token(file_path: str = None):
+def generate_token():
     """
-    Create Token to access your Google-Calendar.
+    Create Token to access your Google-Calendar API.
 
-    Args:
-        file_path (str, optional): file path to your JSON client-secret. If None, defaults to current working directory.
+    Run this command in the directory that contains your Google access credentials.
+    For more help, have a look at the documentation: www.google.com - link is comming soon
     """
-    create_token_from_credentials(file_path=file_path)
+    create_token_from_credentials()
 
 
 @app.command()
 def delete_token():
     """Delete Token if it exists"""
     remove_token()
+
+
+@app.command()
+def cli_test():
+    from gcalcli.calendar.service import create_service
+    service = create_service()
+    all_calendars = service.calendarList().list().execute()
+    for calendar in all_calendars.get("items"):
+        print(calendar.get("summary").lower())
 
