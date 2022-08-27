709963936415aa0c64b87741e40d7ef9196f2e69
Tendai <Tendai.Charura@gmail.com>
2022-08-27 11:27:51 +0200

    Change Token generating process

--- gcalcli/calendar/utils.py
+++ gcalcli/calendar/utils.py
@@ -1,35 +1,33 @@
 import os
-import pickle
 
 from gcalcli.config import settings
 from google_auth_oauthlib.flow import InstalledAppFlow
 
 
-def create_token_from_credentials(file_path: str = None) -> None:
-    """Create and save Token to access Google Calendar from the client-secret JSON.
+def create_token_from_credentials() -> None:
+    """Create and save Token from credentials to access Google Calendar API."""
+    cwd = os.getcwd() 
+    credentials_file = os.path.join(cwd, settings.CREDENTIALS)
 
-    Args:
-        file_path (str, optional): file path to your JSON client-secret. If None, defaults to current working directory.
-    """
-    if not file_path:
-        file_path = os.getcwd() 
-    
-    json_file = os.path.join(file_path, settings.CLIENT_SECRET_JSON)
-
-    if os.path.isfile(json_file):
-        flow = InstalledAppFlow.from_client_secrets_file(json_file, scopes=settings.SCOPES)
-        token = flow.run_console()
-        pickle.dump(token, open(settings.TOKEN, 'wb'))
+    if os.path.isfile(credentials_file):
+        flow = InstalledAppFlow.from_client_secrets_file(
+            credentials_file, settings.SCOPES
+        )
+        creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open(settings.TOKEN, 'w') as token:
+            token.write(creds.to_json())
         print('-' * 30)
         print(f'Token was saved at: {settings.TOKEN}')
     else:
-        print(f'There is no "{settings.CLIENT_SECRET_JSON}" at {file_path}')
+        print(f'There is no "{settings.CREDENTIALS}" at {cwd}')
         print('Please make sure that your JSON file is spelled correctly and is located in the above mentioned directory.')
 
 
 def remove_token():
-    """Delete Token if it had been set in the past with 'gcal set-token'"""
+    """Delete Token if it had been set in the past with 'gcal generate-token'"""
     if os.path.isfile(settings.TOKEN):
         os.remove(settings.TOKEN)
+        print('Your Token has been removed! Before you can use this CLI again, please create a new Token with "gcal generate-token"')
     else:
         print(f'File "{settings.TOKEN}" does not exist!')
