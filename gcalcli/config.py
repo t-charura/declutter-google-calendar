52afb98f16eac9cc1c03c36b9ded511cf52a375d
Tendai <Tendai.Charura@gmail.com>
2022-08-27 11:22:45 +0200

    Changed naming coventions

--- gcalcli/config.py
+++ gcalcli/config.py
@@ -5,10 +5,10 @@ class Settings:
 
     SCOPES = ['https://www.googleapis.com/auth/calendar']
 
-    TOKEN_DIR =  os.path.join(os.path.dirname( __file__ ), '..', 'token')
-    TOKEN_FILE_NAME = 'token.pkl'
-    TOKEN = os.path.join(TOKEN_DIR, TOKEN_FILE_NAME)
+    PROJECT_ROOT_DIR  =  os.path.join(os.path.dirname( __file__ ), '..')
+    TOKEN_FILE_NAME = 'token.json'
+    TOKEN = os.path.join(PROJECT_ROOT_DIR , TOKEN_FILE_NAME)
 
-    CLIENT_SECRET_JSON = 'client_secret.json'
+    CREDENTIALS = 'credentials.json'
 
 settings = Settings()
