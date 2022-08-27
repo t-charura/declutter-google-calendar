571dbdb5ec93efc2af7a6e945b27fa8ccc47babb
Tendai <Tendai.Charura@gmail.com>
2022-08-27 20:20:29 +0200

    Added get app

---
+++ gcalcli/cli/get.py
@@ -0,0 +1,17 @@
+import typer
+
+from gcalcli.calendar.service import create_service
+
+
+get_app = typer.Typer()
+
+
+@get_app.command()
+def calendars():
+    """Show all availabe calendars"""
+    service = create_service()
+    all_calendars = service.calendarList().list().execute()
+    print('-' * 30)
+    for calendar in all_calendars.get("items"):
+        print(calendar.get("summary"))
+
