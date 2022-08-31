25e11495dc60e3a6a83192235468a3ee94a34783
Tendai <Tendai.Charura@gmail.com>
2022-08-31 23:34:53 +0200

    Added arguments to CLI function recurrence

--- gcalcli/cli/delete.py
+++ gcalcli/cli/delete.py
@@ -6,10 +6,13 @@ delete_app = typer.Typer()
 
 
 @delete_app.command()
-def recurrence():
-    """TMP: Delete recurring events"""
+def recurrence(
+    calendar_name: str = typer.Argument(..., help="String to fuzzy match the calendar name"),
+    event_name: str = typer.Argument(..., help="String to fuzzy match the event name"),
+):
+    """Delete instances of recurring events"""
     gcal = Events()
-    gcal.delete_recurring_event_instances(calendar_name="other", event_name="test")
+    gcal.delete_recurring_event_instances(calendar_name=calendar_name, event_name=event_name)
 
 
 @delete_app.command()
