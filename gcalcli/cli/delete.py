49d442c0cf9555021dca0d8518c72a0e97935978
Tendai <Tendai.Charura@gmail.com>
2022-09-10 22:25:44 +0200

    Added date parameter to recurrence

--- gcalcli/cli/delete.py
+++ gcalcli/cli/delete.py
@@ -1,5 +1,8 @@
+from datetime import datetime
+
 import typer
 
+from gcalcli.calendar import utils
 from gcalcli.calendar.delete import Events
 
 delete_app = typer.Typer()
@@ -9,10 +12,15 @@ delete_app = typer.Typer()
 def recurrence(
     calendar_name: str = typer.Argument(..., help="String to fuzzy match the calendar name"),
     event_name: str = typer.Argument(..., help="String to fuzzy match the event name"),
+    date: str = typer.Option(None, "--date", "-d", help="Delete all instances prior to this date (yyyy-mm-dd)"),
 ):
     """Delete instances of recurring events"""
+    if date:
+        date = utils.verify_and_transform_date(date)
+    else:
+        date = datetime.now().isoformat() + "Z"
     gcal = Events()
-    gcal.delete_recurring_event_instances(calendar_name=calendar_name, event_name=event_name)
+    gcal.delete_recurring_event_instances(calendar_name=calendar_name, event_name=event_name, date=date)
 
 
 @delete_app.command()
