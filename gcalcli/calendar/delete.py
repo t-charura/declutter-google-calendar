e4b3914120026c6b786556245e633f1dce0d5810
Tendai <Tendai.Charura@gmail.com>
2022-09-11 16:52:34 +0200

    Moved comments to the end of file

--- gcalcli/calendar/delete.py
+++ gcalcli/calendar/delete.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 from gcalcli.calendar.service import create_service
 from gcalcli.calendar.utils import print_selection, print_time_period
 
@@ -34,10 +32,8 @@ class Events:
                 break
 
     def delete_recurring_event_instances(self, calendar_name: str, event_name: str, date: str):
-
         self._set_calendar_id(calendar_name)
         self._set_event_id(event_name, date)
-
         # get all instances of recurring event
         instances = (
             self.service.events().instances(calendarId=self.calendar_id, eventId=self.event_id, timeMax=date).execute()
@@ -53,31 +49,29 @@ class Events:
             ).execute()
 
     def batch_delete_by_date(self, calendar_name: str, max_date: str, min_date: str = None):
-
         self._set_calendar_id(calendar_name)
-
+        # get all events within the specified time period
         events = (
             self.service.events()
             .list(calendarId=self.calendar_id, singleEvents=True, timeMax=max_date, timeMin=min_date)
             .execute()
         )
-
         # TODO: put into utils - pretty print - number of events / instances
         print("Number of deleted events:", len(events.get("items")))
-
         for event in events.get("items"):
             self.service.events().delete(calendarId=self.calendar_id, eventId=event["id"]).execute()
 
-        # delete all events between 2 dates
-        # delete all events prior to date
-        # if min_date in init
-        # need max_date
-        # if max_date
 
-        # delete ALL events from all calendars
-        # delete all events from calendar x
-        # if calendar_id set in init
+# TODOs for later:
+# delete all events between 2 dates
+# delete all events prior to date
+# if min_date in init
+# need max_date
+# if max_date
 
-        # default: ALL events from all calendars prior to current date
-        # add mind and max date into init
-        pass
+# delete ALL events from all calendars
+# delete all events from calendar x
+# if calendar_id set in init
+
+# default: ALL events from all calendars prior to current date
+# add mind and max date into init
