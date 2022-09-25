8d5a7088f25465d240a5fa793056b98b5c48ee69
Tendai <Tendai.Charura@gmail.com>
2022-09-25 12:46:06 +0700

    Added GetInfo

---
+++ gcalcli/calendar/get.py
@@ -0,0 +1,24 @@
+from gcalcli.calendar import CalendarBase, utils
+
+
+class GetInfo(CalendarBase):
+    def get_calendars(self):
+        """Show all availabe calendars."""
+        all_calendars = self.service.calendarList().list().execute()
+        print("Availabe calendars:")
+        for calendar in all_calendars.get("items"):
+            print("*", calendar.get("summary"))
+
+    def get_events_from_calendar(self, calendar_name: str):
+        """Show events from a specific calendar (max 250 entries).
+
+        Args:
+            calendar_name (str): Select a specific calendar
+        """
+        self._set_calendar_id(calendar_name)
+        events = self.service.events().list(calendarId=self.calendar_id).execute()
+        for event in events.get("items"):
+            summary = event.get("summary")
+            recurrence = event.get("recurrence")
+            if summary:
+                utils.print_event_names(summary, recurrence)
